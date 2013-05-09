274b7b1b47d83ad4f937c9024775eb9ff8c8ab93
Johnny <u1067159@campus.udg.edu>
2013-05-09 17:02:34 +0200

    mongouri

--- routes/admin.js
+++ routes/admin.js
@@ -11,11 +11,11 @@ var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHOW_URL || 'mongodb:
 
 mongo.Db.connect(mongoUri, function (err, db) {
   if (err) {
-    console.log("Error 1:" + mongoUri);
+    console.log("Error 1: " + mongoUri);
   }
   db.collection('mydocs', function(er, collection) {
     if (er) {
-      console.log("Error 2": + mongoUri);
+      console.log("Error 2: " + mongoUri);
     }
     collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
     });
