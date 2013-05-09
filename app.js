3a61827a21634c27b1c0384a91c3c5f189a81efc
johnny32 <u1067159@campus.udg.edu>
2013-05-09 20:49:47 +0200

    Recuperat el merge del desastre que va ocòrrer el dia Dijous 9 de maig de 2013 de 15:00 a 18:20 a classe d'ADA.

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var express = require('express')
   , web = require('./routes/web')
   , ingredients = require('./routes/ingredients')
   , http = require('http')
-  , path = require('path'); 
+  , path = require('path');
 
 var app = express();
 
@@ -20,7 +20,7 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
-app.use(express.logger());
+app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(express.cookieParser());
--- routes/admin.js
+++ routes/admin.js
@@ -7,21 +7,23 @@
  */
 var mongo = require('mongodb');
 
-var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHOW_URL || 'mongodb://localhost/sinatra';
+var Server = mongo.Server,
+    Db = mongo.Db,
+    BSON = mongo.BSONPure;
 
-mongo.Db.connect(mongoUri, function (err, db) {
-  if (err) {
-    console.log("Error 1: " + mongoUri);
-  }
-  db.collection('mydocs', function(er, collection) {
-    if (er) {
-      console.log("Error 2: " + mongoUri);
-    }
-    collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
+var server = new Server('localhost', 27017, {auto_reconnect: true});
+db = new Db('sinatra', server);
+
+db.open(function(err, db) {
+  if (!err) {
+    db.collection('users', {strict: true}, function(err, collection) {
+      if (err) {
+        console.log("The 'reg_users' collection doesn't exist. Creating it with sample data...");
+        populateDB();
+      }
     });
-  });
+  }
 });
-
 
 /**
  * Accedeix a la part de backend (si l'admin ha iniciat sessio) o renderitza la pagina de login
@@ -53,7 +55,6 @@ exports.index = function(req, res) {
  * @date    2013-04-28
  */
 exports.loginPage = function(req, res) {
-  console.log("HOLLAAAAAVAJHDVASGUDVGASKDVASGKJDVGJASVDKGJASVDKJASG·KDJA");
   if (req.session.logged) {
     res.redirect('/admin');
   }
@@ -82,10 +83,7 @@ exports.loginAction = function(req, res) {
   var pass = req.body.passenc;
   console.log('Retrieving admin user: ' + user);
   console.log('Encrypted password: ' + pass);
-  mongo.Db.collection('reg_users', function(err, collection) {
-    if (err) {
-      console.log("Errorrrrrrrrrrrrrrrrrrrrrrrrr: " + mongoUri);
-    }
+  db.collection('reg_users', function(err, collection) {
     collection.findOne({'user': user, 'pass': pass}, function(err, item) {
       if (!err && item != null) {
         console.log("Correct admin user: " + req.body.user);
