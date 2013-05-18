139831dc9951f46a4ca415b14b610af7ec7824f8
johnny32 <u1067159@campus.udg.edu>
2013-05-18 16:59:49 +0200

    Pàgina principal

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@
  * GET home page.
  */
 
+/**
+ * Mostra la pagina principal.
+ *
+ * @param req
+ * @param res
+ *
+ * @author  jclara
+ * @version 1.0
+ * @date    2013-05-18
+ */
 exports.index = function(req, res){
-  //TODO Crear pagina principal
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Sinatra Cockteleria' });
 };
